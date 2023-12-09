b425bed142505ed09f03ec5b6c208d497705379f
lucasfernande <lucas.fernandes9415@outlook.com>
2023-12-08 22:34:02 -0300

    testando laço for each

---
+++ Secao10/src/foreach/ForEach.java
@@ -0,0 +1,11 @@
+package foreach;
+
+public class ForEach {
+    public static void main(String[] args) {
+        String nomes[] = new String[] {"João", "Pedro", "Maria"};
+
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+    }
+}
