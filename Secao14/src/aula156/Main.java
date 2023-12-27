b9a2ab94743c1695320228888b3fe35ae1aed9f7
lucasfernande <lucas.fernandes9415@outlook.com>
2023-12-27 16:15:17 -0300

    entendendo upcasting e downcasting

--- Secao14/src/aula156/Main.java
+++ Secao14/src/aula156/Main.java
@@ -1,12 +1,35 @@
 package aula156;
 
+import aula156.entities.Account;
 import aula156.entities.BusinessAccount;
+import aula156.entities.SavingsAccount;
 
 public class Main {
     public static void main(String[] args) {
-        BusinessAccount account = new BusinessAccount();
-        account.getBalance();
 
-        // o método getBalance foi definido apenas na classe Account, então a BusinessAccount herdou ele
+        Account acc = new Account(1001, "Alex", 0.0);
+        Account bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+        Account sacc = new SavingsAccount(1003, "Bob", 100.0, 0.1);
+
+        // upcasting -> casting da subclasse para superclasse
+        Account accTeste = bacc; // isso é possível devido a BusinessAccount ser também uma Account
+
+        // downcasting -> casting da superclasse para subclasse
+        BusinessAccount baccTeste = (BusinessAccount) accTeste; // nesse caso é necessário fazer o casting manual
+
+        // BusinessAccount saccTeste = (BusinessAccount) sacc; // isso não é possível, pois a SavingsAccount não é uma BusinessAccount
+
+        if (sacc instanceof BusinessAccount) {
+            BusinessAccount saccTeste = (BusinessAccount) sacc;
+            saccTeste.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (sacc instanceof SavingsAccount) {
+            SavingsAccount saccTeste = (SavingsAccount) sacc;
+            saccTeste.updateBalance();
+            System.out.println("Update!");
+        }
+
     }
 }
---
+++ Secao14/src/aula156/entities/SavingsAccount.java
@@ -0,0 +1,27 @@
+package aula156.entities;
+
+public class SavingsAccount extends Account {
+
+    private Double interestRate;
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void updateBalance() {
+        deposit(this.getBalance() * interestRate);
+    }
+}
