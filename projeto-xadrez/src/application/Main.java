c981d7e437c2d006afb7895db9e7de43d3d93ffd
lucasfernande <lucas.fernandes9415@outlook.com>
2024-07-25 20:56:10 -0300

    lógica de cheque mate

--- projeto-xadrez/src/application/Main.java
+++ projeto-xadrez/src/application/Main.java
@@ -16,7 +16,7 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -47,5 +47,8 @@ public class Main {
                 scan.nextLine();
             }
         }
+
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
