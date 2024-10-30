66e6f15617c2754e2b000bc1149d16336d82b917
lucasfernande <lucas.fernandes9415@outlook.com>
2024-10-30 09:23:47 -0300

    utilizando try-with-resources

--- Secao15b/src/aula156/Main.java
+++ Secao15b/src/aula156/Main.java
@@ -8,13 +8,8 @@ public class Main {
     public static void main(String[] args) {
 
         String path = "C:\\temp\\teste.txt";
-        BufferedReader br = null;
-        FileReader fr = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine(); // lê uma linha e, se estiver no final, retorna nulo
 
             while (line != null) {
@@ -23,17 +18,6 @@ public class Main {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
